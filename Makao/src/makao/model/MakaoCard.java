e6be19e6bda5640d86df61b02258624b1ffceeae
Przemek <przlada@gmail.com>
2014-01-25 17:44:45 +0100

    Królowie zgodnie z zasadami, przed przymiarką to polskich znaków

--- Makao/src/makao/model/MakaoCard.java
+++ Makao/src/makao/model/MakaoCard.java
@@ -5,6 +5,26 @@ import java.io.Serializable;
 public class MakaoCard implements Serializable {
 	private final int number;
 	private final int color;
+	
+	public static final int CARD_NO_2 = 1;
+	public static final int CARD_NO_3 = 2;
+	public static final int CARD_NO_4 = 3;
+	public static final int CARD_NO_5 = 4;
+	public static final int CARD_NO_6 = 5;
+	public static final int CARD_NO_7 = 6;
+	public static final int CARD_NO_8 = 7;
+	public static final int CARD_NO_9 = 8;
+	public static final int CARD_NO_10 = 9;
+	public static final int CARD_JACK = 10;
+	public static final int CARD_QUEEN = 11;
+	public static final int CARD_KING = 12;
+	public static final int CARD_ACE = 0;
+	
+	public static final int COLOR_CLUBS = 0; //trefl
+	public static final int COLOR_DIAMONDS = 3; //karo
+	public static final int COLOR_HEARTS = 2; //kier
+	public static final int COLOR_SPADES = 1; //pik
+	
 	/**
 	 * 
 	 * @param number figura karty
