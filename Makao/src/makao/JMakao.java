28daa1f4e87d2799749d330b62b09ad70f25be67
Przemek <przlada@gmail.com>
2014-01-26 21:22:00 +0100

    Komentarze JMakao

--- Makao/src/makao/JMakao.java
+++ Makao/src/makao/JMakao.java
@@ -1,61 +1,25 @@
 package makao;
-import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import makao.controller.Client;
 import makao.controller.Controller;
 import makao.model.Model;
-import makao.view.MainView;
 import makao.view.View;
 import makao.view.actions.MakaoActions;
 
-
+/**
+ * G¸—wna klasa aplikacji tworzˆca Model Widok i Kontroler
+ * @author przemek
+ */
 public class JMakao {
 	public static void main(String[] args) {
-		/*
-		final BlockingQueue<String> bq = new LinkedBlockingQueue<String>();
-		Model model = new Model();
-		View widok = new View(bq);
-		Controller controler = new Controller(bq, model, widok);
-		model.setController(controler);
-		controler.start();
-		*/
+		
 		final BlockingQueue<MakaoActions> actionQueue = new LinkedBlockingQueue<MakaoActions>();
 		View view = new View(actionQueue);
 		Model model = new Model();
 		Controller controller = new Controller(view, model, actionQueue);
 		model.setController(controller);
 		controller.start();
-		//while(true){}
-		
-		// TODO Auto-generated method stub
-		/*
-		MainView main = new MainView();
-		main.setVisible(true);
-		System.out.println("SERWER CZY KLIENT?");
-		Scanner sc = new Scanner (System.in);
-	    String info = sc.nextLine();
-	    System.out.println(info);
-
-	    if(info.equals("s")){
-	    	Server server = new Server(3456);
-	    	server.startServer();
-	    }
-	    else{
-	    	Client client = new Client("localhost", 3456);
-	    	System.out.println(client.connect());
-	    	while(!info.equals("q")){
-	    		info = sc.nextLine();
-	    		client.send(info);
-	    		System.out.println(client.getMessage());
-	    	}
-	    	System.out.println("Disconnect");
-	    	client.disconnect();
-	    }
-	    
-	    */
-		
 	}
 
 }
